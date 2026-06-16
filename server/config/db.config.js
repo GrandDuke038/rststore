a9574d8a1c859909a5a1d6458cc8920f9bb05d23
GrandDuke038 <isaacnyoike018@gmail.com>
2026-06-16 20:33:00 +0300

    created seeder file installed colors package commands added models folder in the jsconfig

--- server/config/db.config.js
+++ server/config/db.config.js
@@ -1,12 +1,13 @@
 import mongoose from "mongoose";
+import colors from "colors";
 
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
 
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    console.log(`MongoDB Connected: ${conn.connection.host}`.blue.bold.inverse);
   } catch (error) {
-    console.error(`Error: ${error.message}`);
+    console.error(`Error: ${error.message}`.red.underline);
     process.exit(1);
   }
 };
--- server/main.js
+++ server/main.js
@@ -1,8 +1,10 @@
 import express from "express";
+import colors from "colors";
+import morgan from "morgan";
 
 import dotenv from "dotenv";
 
-import products from "#data/products.js";
+import products from "#data/products.data.js";
 import connectDB from "#config/db.config.js";
 
 dotenv.config();
@@ -11,6 +13,7 @@ const port = process.env.PORT;
 connectDB();
 
 const app = express();
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   res.send("API is running.....");
@@ -26,6 +29,7 @@ app.get("/api/v1/products/:id", (req, res) => {
 
 app.listen(port, () => {
   console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port: ${port}`,
+    `Server running in ${process.env.NODE_ENV} mode on port: ${port}`.yellow
+      .bold,
   );
 });
