0540ffbba53a7832cec487ce75d259366b021535
GrandDuke038 <isaacnyoike018@gmail.com>
2026-06-17 14:27:58 +0300

    Refactor product routes to use controllers
    Separates business logic from route definitions by introducing a controller layer to improve code organization and maintainability.
    
    Updates path alias configurations to support the new directory structure and simplifies route definitions using the router.route method.

---
+++ server/controllers/product.controller.js
@@ -0,0 +1,14 @@
+import ProductModel from "#models/product.model.js";
+const getProducts = async (req, res) => {
+  const products = await ProductModel.find({});
+  res.json(products);
+};
+
+const getProductsById = async (req, res) => {
+  const product = await ProductModel.findById(req.params.id);
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404).json({ message: "Product not found" });
+  }
+};
--- server/routes/product.routes.js
+++ server/routes/product.routes.js
@@ -1,18 +1,12 @@
+import {
+  getProducts,
+  getProductsById,
+} from "#controllers/product.controller.js";
 import express from "express";
-import ProductModel from "#models/product.model.js";
 const router = express.Router();
 
-router.get("/", async (req, res) => {
-  const products = await ProductModel.find({});
-  res.json(products);
-});
-router.get("/:id", async (req, res) => {
-  const product = await ProductModel.findById(req.params.id);
-  if (product) {
-    res.json(product);
-  } else {
-    res.status(404).json({ message: "Product not found" });
-  }
-});
+router.route("/").get(getProducts);
+
+router.route("/:id").get(getProductsById).delete(deleteProduct).put();
 
 export default router;
