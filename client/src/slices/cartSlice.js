9bd71c98c231fd045881bc8eb4a74927d08f12bb
GrandDuke038 <isaacnyoike018@gmail.com>
2026-06-18 16:26:19 +0300

    Add addToCart reducer with price calculations
    Enables adding or updating items within the shopping cart while automatically calculating subtotal, shipping, tax, and total costs.
    
    Persists the updated cart state to local storage to ensure data is maintained across browser sessions.

--- client/src/slices/cartSlice.js
+++ client/src/slices/cartSlice.js
@@ -7,7 +7,36 @@ const initialState = localStorage.getItem("cart")
 const cartSlice = createSlice({
   name: "cart",
   initialState,
-  reducers: {},
-});
+  reducers: {
+    addToCart: (state, action) => {
+      const item = action.payload;
+      const existingItem = state.cartItems.find(
+        (cartItem) => cartItem._id === item._id,
+      );
+      if (existingItem) {
+        state.cartItem = state.cartItem.map((cartItem) => {
+          return cartItem._id === existingItem._id ? item : cartItem;
+        });
+      } else {
+        state.cartItems = [...state.cartItems, item];
+      }
+      //calculate items prices
+      state.itemsPrice = state.cartItems.reduce(
+        (acc, currVal) => acc + currVal.price * currVal.qty,
+      );
+      //calculate shipping price
+      state.shippingPrice = state.itemPrice > 5000 ? 0 : 2000;
 
+      //calculate tax price
+      state.taxPrice = 0.18 * state.itemsPrice;
+
+      //calculate total price
+      state.totalPrice =
+        state.itemsPrice + state.shippingPrice + state.taxPrice;
+
+      localStorage.setItem("cart", JSON.stringify(state));
+    },
+  },
+});
+export const { addToCart } = cartSlice.actions;
 export default cartSlice.reducer;
