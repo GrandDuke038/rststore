aecb85035efeba46ff6f7aa3a249e6c0b60c6a19
GrandDuke038 <isaacnyoike018@gmail.com>
2026-06-23 16:23:54 +0300

    Implement JWT authentication via secure cookies
    Generates a JSON Web Token upon successful user authentication and stores it in an HTTP-only cookie. This improves security by protecting the token from cross-site scripting attacks and simplifies session management on the client side.

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,3 +1,4 @@
+import jwt from "jsonwebtoken";
 import userModel from "#models/user.model.js";
 
 /**
@@ -12,6 +13,16 @@ const authUser = async (req, res) => {
   const user = await userModel.findOne({ email });
 
   if (user && (await user.matchPassword(password))) {
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "30d",
+    });
+    res.cookie("jwt", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV !== "development",
+      sameSite: "strict",
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+    });
+
     res.json({
       _id: user._id,
       name: user.name,
