671aaefeac8bd6fdfd67781ad7c0405868eaa57f
GrandDuke038 <isaacnyoike018@gmail.com>
2026-06-24 16:52:55 +0300

    Implement authentication state and profile logic
    Establishes Redux state management for user authentication, allowing credentials to be persisted in local storage.
    
    Adds frontend API integration for user login and implements backend controller logic to handle fetching and updating user profile information, including secure password updates and token regeneration.

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -78,7 +78,18 @@ const logoutUser = async (req, res) => {
  */
 
 const getUserProfile = async (req, res) => {
-  res.send("Get user profile");
+  const user = await userModel.findById(req.user._id);
+  if (user) {
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
 };
 /**
  * @desc  Update user profile
@@ -87,7 +98,27 @@ const getUserProfile = async (req, res) => {
  */
 
 const updateUserProfile = async (req, res) => {
-  res.send("Update user profile");
+  const user = await userModel.findById(req.user._id);
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+    const updatedUser = await user.save();
+    generateToken(res, updatedUser._id);
+
+    res.status(200).json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
 };
 /**
  * @desc  Get all users
