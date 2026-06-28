8b2a90898665a37740d97b08702a8b0af73c410e
GrandDuke038 <isaacnyoike018@gmail.com>
2026-06-28 09:16:22 +0300

    Finished the get order by id controller, and modified its route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,6 +14,7 @@ import ShippingScreen from "@screens/Shipping";
 import PrivateRoute from "@components/PrivateRoute";
 import PaymentScreen from "@screens/Payment";
 import PlaceOrderScreen from "@screens/PlaceOrder";
+import OrderScreen from "@screens/Order";
 
 const router = createBrowserRouter([
   {
@@ -35,6 +36,7 @@ const router = createBrowserRouter([
       },
       { path: "/payment", element: <PaymentScreen /> },
       { path: "/place-order", element: <PlaceOrderScreen /> },
+      { path: "/order/:id", element: <OrderScreen /> },
       {
         path: "*",
         element: <ErrorScreen />,
