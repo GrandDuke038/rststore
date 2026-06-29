1ae166f8aff3fb5220fdcb258cd3eac5c3ce13a7
GrandDuke038 <isaacnyoike018@gmail.com>
2026-06-29 12:12:15 +0300

    Adds order history to profile screen
    Integrates a new section on the user profile page to display a list of past orders.
    
    Introduces a new API endpoint and RTK Query hook to fetch the authenticated user's order history. The UI displays key order details, individual order items, and provides links to view full order details and product pages. Includes loading and error states for a better user experience.

--- client/src/screens/Profile/index.jsx
+++ client/src/screens/Profile/index.jsx
@@ -2,8 +2,12 @@ import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { toast } from "react-toastify";
 
+import Alert from "@components/Alert";
+import Loader from "@components/Header/Loader";
 import { setCredentials } from "@slices/authSlice";
+import { useGetMyOrdersQuery } from "@slices/orderApiSlice";
 import { useProfileMutation } from "@slices/userApiSlice";
+import { Link } from "react-router-dom";
 
 const ProfileScreen = () => {
   const [name, setName] = useState("");
@@ -13,9 +17,14 @@ const ProfileScreen = () => {
 
   const dispatch = useDispatch();
   const { userInfo } = useSelector((state) => state.auth);
-
   const [updateProfile, { isLoading: loadingUpdateProfile }] =
     useProfileMutation();
+
+  const {
+    data: orders,
+    isLoading: loadingOrders,
+    error: errorOrders,
+  } = useGetMyOrdersQuery();
 
   useEffect(() => {
     if (userInfo) {
@@ -80,7 +89,6 @@ const ProfileScreen = () => {
                   />
                 </div>
               </div>
-
               <div className="sm:col-span-full">
                 <label
                   htmlFor="email"
@@ -145,6 +153,143 @@ const ProfileScreen = () => {
             </button>
           </div>
         </form>
+
+        <div className="mt-20 max-w-xl">
+          <h1 className="text-2xl font-bold tracking-tight text-slate-900 sm:text-2xl">
+            Order history
+          </h1>
+        </div>
+
+        {loadingOrders ? (
+          <Loader />
+        ) : errorOrders ? (
+          <Alert type="error">
+            {errorOrders?.data?.message || errorOrders?.message}
+          </Alert>
+        ) : (
+          <div className="mt-16">
+            <h2 className="sr-only">Recent orders</h2>
+
+            <div className="space-y-20">
+              {orders?.map((order, i) => (
+                <div key={order._id}>
+                  <div className="rounded-lg bg-slate-50 px-4 py-6 sm:flex sm:items-center sm:justify-between sm:space-x-6 sm:px-6 lg:space-x-8">
+                    <dl className="flex flex-auto justify-between space-y-6 divide-y divide-slate-200 text-sm text-slate-600 sm:gap-x-6 sm:space-y-0 sm:divide-y-0 lg:w-full lg:flex-none lg:gap-x-8">
+                      <div className="flex justify-between pt-6 sm:block sm:pt-0">
+                        <dt className="font-medium text-slate-900">
+                          <span className="opacity-50">#{i + 1}</span> &nbsp;
+                          Order ID
+                        </dt>
+                        <dd className="sm:mt-1">{order._id}</dd>
+                      </div>
+                      <div className="flex justify-between sm:block">
+                        <dt className="font-medium text-slate-900">
+                          Date placed
+                        </dt>
+                        <dd className="sm:mt-1">
+                          <time dateTime={order.createdAt}>
+                            {new Date(order.createdAt).toLocaleDateString()}
+                          </time>
+                        </dd>
+                      </div>
+                      <div className="flex justify-between pt-6 sm:block sm:pt-0">
+                        <dt className="font-medium text-slate-900">
+                          Delivered At
+                        </dt>
+                        <dd className="sm:mt-1">
+                          {order.deliveredAt || "Pending Delivery"}
+                        </dd>
+                      </div>
+                      <div className="flex justify-between pt-6 font-medium text-slate-900 sm:block sm:pt-0">
+                        <dt>Total amount</dt>
+                        <dd className="sm:mt-1">₹{order.totalPrice}</dd>
+                      </div>
+
+                      <Link
+                        to={`/order/${order._id}`}
+                        className="mt-6 flex w-full items-center justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:w-auto"
+                      >
+                        View Order Details
+                      </Link>
+                    </dl>
+                  </div>
+
+                  <table className="mt-4 w-full text-slate-500 sm:mt-6">
+                    <caption className="sr-only">Products</caption>
+                    <thead className="sr-only text-left text-sm text-slate-500 sm:not-sr-only">
+                      <tr>
+                        <th
+                          scope="col"
+                          className="py-2 pr-8 font-normal sm:w-2/5 lg:w-1/3"
+                        >
+                          Product
+                        </th>
+                        <th
+                          scope="col"
+                          className="hidden w-1/5 py-2 pr-8 font-normal sm:table-cell"
+                        >
+                          Price
+                        </th>
+                        <th
+                          scope="col"
+                          className="hidden w-1/5 py-2 pr-8 font-normal sm:table-cell"
+                        >
+                          Quantity
+                        </th>
+                        <th
+                          scope="col"
+                          className="w-0 py-2 text-right font-normal"
+                        >
+                          <span className="sr-only">Info</span>
+                        </th>
+                      </tr>
+                    </thead>
+                    <tbody className="divide-y divide-slate-200 border-b border-slate-200 text-sm sm:border-t">
+                      {order?.orderItems?.map((item) => (
+                        <tr key={item.product}>
+                          <td className="py-3 pr-8">
+                            <div className="flex items-center">
+                              <img
+                                alt={item.src}
+                                src={item.image}
+                                className="mr-6 h-16 w-16 rounded object-cover object-center"
+                              />
+                              <div>
+                                <div className="font-medium text-slate-900">
+                                  {item.name}
+                                </div>
+                                <div className="mt-1 sm:hidden">
+                                  ₹{item.price}
+                                </div>
+                              </div>
+                            </div>
+                          </td>
+                          <td className="hidden py-3 pr-8 sm:table-cell">
+                            ₹{item.price}
+                          </td>
+                          <td className="hidden py-3 pr-8 sm:table-cell">
+                            <strong>{item.qty}</strong> x ₹{item.price} = ₹
+                            {item.qty * item.price}
+                          </td>
+                          <td className="whitespace-nowrap py-3 text-right font-medium">
+                            <Link
+                              to={`/product/${item.product}`}
+                              className="text-indigo-600"
+                            >
+                              View
+                              <span className="hidden lg:inline"> Item</span>
+                              <span className="sr-only">, {item.name}</span>
+                            </Link>
+                          </td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
